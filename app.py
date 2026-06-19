51d74b2ffd52e8cf9c2985d7ae28d165aca99ee2
om <ommasal1311@gmail.com>
2026-06-19 17:48:59 +0530

    Added weather status and Chart.js visualization

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import joblib
 
 app = Flask(__name__)
@@ -7,22 +7,31 @@ model = joblib.load("model.pkl")
 
 @app.route("/")
 def home():
-    return "Weather Prediction API Running"
-
+    return render_template("index.html")
 @app.route("/predict", methods=["POST"])
 def predict():
 
-    data = request.get_json()
-
-    temperature = data["temperature"]
-    humidity = data["humidity"]
+    temperature = float(request.form["temperature"])
+    humidity = float(request.form["humidity"])
 
     prediction = model.predict([[temperature, humidity]])
 
-    return jsonify({
-        "predicted_temperature": round(float(prediction[0]), 2)
-    })
+    predicted_temp = round(float(prediction[0]), 2)
+
+    if predicted_temp > 35:
+        weather_status = "🔥 Hot"
+    elif predicted_temp > 25:
+        weather_status = "🌤 Pleasant"
+    else:
+        weather_status = "❄ Cool"
+
+    return render_template(
+        "index.html",
+        prediction=predicted_temp,
+        temp=temperature,
+        humidity=humidity,
+        weather_status=weather_status
+    )
 
 if __name__ == "__main__":
- if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
